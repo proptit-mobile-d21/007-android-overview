13930071376d749a8e1aca8116625b24ddf8102c
mdunggggg <dungmanh74@gmail.com>
2023-07-13 13:47:15 +0700

    test

--- app/src/main/java/dev/proptit/androidoverview/MainActivity.kt
+++ app/src/main/java/dev/proptit/androidoverview/MainActivity.kt
@@ -2,10 +2,16 @@ package dev.proptit.androidoverview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main)
+
+
     }
 }
