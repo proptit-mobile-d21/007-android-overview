74ed7b5c236ac90c1fc9226a44958a30e963b130
Zero-zx <dthieu2512@gmail.com>
2023-07-14 01:04:46 +0700

    commit

--- app/src/main/java/dev/proptit/androidoverview/MainActivity.kt
+++ app/src/main/java/dev/proptit/androidoverview/MainActivity.kt
@@ -1,11 +1,41 @@
 package dev.proptit.androidoverview
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.SpannedString
+import android.text.style.ForegroundColorSpan
+import dev.proptit.androidoverview.databinding.ActivityMainBinding
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val spannedString = SpannableString("Let's Enjoy Your Dream Vacation")
+        val index1 = spannedString.indexOf("Enjoy")
+        val index2 = spannedString.indexOf("Dream")
+
+        // Set the text color of the word
+        val color = resources.getColor(R.color.dark_green)
+        val span1 = ForegroundColorSpan(color)
+        val span2 = ForegroundColorSpan(color)
+        spannedString.setSpan(span1, index1, index1 + 5, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannedString.setSpan(span2, index2, index2 + 14, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        // Set the text of the TextView to the SpannableString
+        binding.tvHeading.setText(spannedString)
+
+
+        val spannedString2 = SpannableString("Already have an account? Sign in")
+        val index3 = spannedString2.indexOf("Sign")
+        val span3 = ForegroundColorSpan(color)
+        spannedString2.setSpan(span3, index3, index3 + 7, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.tvSignin.setText(spannedString2)
+
     }
 }
