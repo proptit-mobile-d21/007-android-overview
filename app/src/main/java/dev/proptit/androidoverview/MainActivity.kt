67c05a8ada015ddcd36680d6622d7e89d27ee5ab
ProJulius <toanquen1314@gmail.com>
2023-07-13 13:12:39 +0700

    TrungLQ

--- app/src/main/java/dev/proptit/androidoverview/MainActivity.kt
+++ app/src/main/java/dev/proptit/androidoverview/MainActivity.kt
@@ -2,8 +2,10 @@ package dev.proptit.androidoverview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
