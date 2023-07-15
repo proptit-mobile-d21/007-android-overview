b3d0309846ed18817b1891dad6ad51ccacd26d83
Zero-zx <dthieu2512@gmail.com>
2023-07-15 07:45:05 +0700

    Updated

--- app/src/main/java/dev/proptit/androidoverview/MainActivity.kt
+++ app/src/main/java/dev/proptit/androidoverview/MainActivity.kt
@@ -1,14 +1,11 @@
 package dev.proptit.androidoverview
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.SpannableString
-import android.text.SpannedString
 import android.text.style.ForegroundColorSpan
+import android.util.Log
 import dev.proptit.androidoverview.databinding.ActivityMainBinding
-import java.util.zip.Inflater
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,27 +13,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        val spannedString = SpannableString("Let's Enjoy Your Dream Vacation")
-        val index1 = spannedString.indexOf("Enjoy")
-        val index2 = spannedString.indexOf("Dream")
-
-        // Set the text color of the word
         val color = resources.getColor(R.color.dark_green)
-        val span1 = ForegroundColorSpan(color)
-        val span2 = ForegroundColorSpan(color)
-        spannedString.setSpan(span1, index1, index1 + 5, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannedString.setSpan(span2, index2, index2 + 14, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
-
-        // Set the text of the TextView to the SpannableString
-        binding.tvHeading.text = spannedString
-
-
-        val spannedString2 = SpannableString(binding.tvSignin.text)
-        val index3 = spannedString2.indexOf("Sign")
-        val span3 = ForegroundColorSpan(color)
-        spannedString2.setSpan(span3, index3, index3 + 7, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
-        binding.tvSignin.setText(spannedString2)
-        //setTextColorForParts(binding.tvSignin, binding.tvSignin.text.toString(), "Sign in", color)
+        setTextColorForParts(binding.tvHeading , getString(R.string.highlight_heading_1), color)
+        setTextColorForParts(binding.tvHeading , getString(R.string.highlight_heading_2), color)
+        setTextColorForParts(binding.tvSignin , getString(R.string.highlight_signin), color)
     }
 }
--- app/src/main/java/dev/proptit/androidoverview/setColorForParts.kt
+++ app/src/main/java/dev/proptit/androidoverview/setColorForParts.kt
@@ -1,14 +1,17 @@
 package dev.proptit.androidoverview
 
 import android.text.SpannableString
-import android.text.Spanned
 import android.text.style.ForegroundColorSpan
+import android.util.Log
 import android.widget.TextView
 
-fun setTextColorForParts(textView: TextView, fullText: String, text: String, color: Int) {
-    val spannableString = SpannableString(fullText)
+fun setTextColorForParts(textView: TextView, text: String, color: Int) {
+    val spannableString = SpannableString(textView.text)
     val index = spannableString.indexOf(text)
     val span = ForegroundColorSpan(color)
+//    Log.d("setColor", spannableString.toString())
+//    Log.d("setColor", text.toString())
+//    Log.d("setColor",index.toString())
     spannableString.setSpan(span, index, index + text.length, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
     textView.text = spannableString
 }
